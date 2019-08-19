fbb142dfd135bfeef9c1fbf787f7ded7d2f4d6d1
Travis Nelson <tnelson17@gmail.com>
2019-08-18 20:34:58 -0400

    FAT YIKES MY DOGGY

--- src/Square.js
+++ src/Square.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import './Square.css'
 
-const Square = () => {
+const Square = (props) => {
+console.log(props);
+
+// const handleClick = () => {
+//   props.handleChange(props.location)
+// }
+
+const {handleChange} = props;
+console.log(handleChange);
+
   return(
-    <td className="square">
+    <td className="square" onClick={handleChange}>
       &nbsp;
     </td>
   )
